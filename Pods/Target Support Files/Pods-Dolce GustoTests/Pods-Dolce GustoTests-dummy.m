b4d2e35c033b4dc4e7f38bf935ade88faeb57a4b
mattcbr <matheus.castelo@gmail.com>
2018-11-11 15:56:45 -0200

    Adding cocoapods and FMDB

---
+++ Pods/Target Support Files/Pods-Dolce GustoTests/Pods-Dolce GustoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dolce_GustoTests : NSObject
+@end
+@implementation PodsDummy_Pods_Dolce_GustoTests
+@end
---
+++ Pods/Target Support Files/Pods-Dolce GustoUITests/Pods-Dolce GustoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dolce_GustoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dolce_GustoUITests
+@end
